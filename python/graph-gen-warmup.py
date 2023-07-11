9b6fc53570f91aecce14574b6871ccbecfb90853
Kassouley <lucas.real.neto@gmail.com>
2023-07-11 16:35:22 +0200

    calibrate

---
+++ python/graph-gen-warmup.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+import sys
+import datetime as dt
+
+def read_data(file): 
+    step=[]
+    median=[]
+    stability=[]
+    line = file.readline()
+    while True:
+        line = file.readline()
+        if not line:
+                break
+        line = line.replace('\n', '')
+        line = line.replace(' ', '')
+        line = line.split(sep="|")
+        step.append(line[0])
+        median.append(int(line[1]))
+        stability.append(float(line[2]))
+    return step, median, stability
+
+
+def main() :
+    file = open(sys.argv[2])
+    step, median, stability = read_data(file)
+
+    fig, ax1 = plt.subplots(1,1)
+
+    # formatter = ticker.ScalarFormatter()
+    # formatter.set_scientific(False)
+    # ax1.yaxis.set_major_formatter(formatter)
+
+    ax2 = ax1.twinx()
+    ax1.bar(label, median)
+    ax1.set_ylabel(f'{sys.argv[4]} median')
+    ax2.plot(stability, color='y')
+    ax2.set_ylabel('Stability')
+    ax2.yaxis.set_major_formatter(ticker.PercentFormatter())
+    ax1.set_xticks(label)
+    ax1.set_xticklabels(label,rotation=30, ha='right')
+    ax1.set_title(f"Kernel Performances for a {sys.argv[1]}x{sys.argv[1]} matrix")
+    plt.tight_layout()
+    date = f"{dt.datetime.now()}".replace(' ', '-', 1)
+    plt.savefig(sys.argv[3])
+
+if __name__ == '__main__':
+    main()
+
---
+++ src/calibrate/driver_calib.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include "kernel.h"
+#include "tab.h"
+
+#define NB_META 31
+#define OUTOUT_FILE "output_calibrate.txt"
+
+extern uint64_t rdtsc ();
+
+static int cmp_uint64 (const void *a, const void *b)
+{
+    const uint64_t va = *((uint64_t *) a);
+    const uint64_t vb = *((uint64_t *) b);
+
+    if (va < vb) return -1;
+    if (va > vb) return 1;
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    unsigned int n, repm;
+    char* file_name = NULL;
+    FILE * output = NULL;
+    if (argc != 2 && argc != 3) 
+    {
+        fprintf (stderr, "Usage: %s <problem size> <nb repeat> [file name]\n", argv[0]);
+        return 1;
+    }
+    else
+    {
+        n = atoi(argv[1]);
+        repm = atoi(argv[2]);
+        file_name = (char*)malloc(256*sizeof(char));
+        if (argc == 2)
+            strcpy(file_name, OUTOUT_FILE);
+        else if (argc == 3)
+            strcpy(file_name, argv[2]);
+    }
+
+    uint64_t **tdiff = malloc( repm * sizeof(tdiff[0][0]));
+    for(unsigned int k = 0 ; k < repm ; k++)
+    {
+        tdiff[k] = malloc( NB_META * sizeof(tdiff[0]));
+    }
+
+    int size = n * n * sizeof(double);
+    
+    double *a = (double*)malloc(size);
+    double *b = (double*)malloc(size);
+    double *c = (double*)malloc(size);
+
+    srand(0);
+    init_tab2d_random(n, &b);
+    init_tab2d_random(n, &c);
+    
+    for (unsigned int m = 0; m < NB_META; m++)
+    {
+        for (unsigned int k = 0; k < repm; k++)
+        {
+            const uint64_t t1 = rdtsc();
+            kernel(n, a, b, c);
+            const uint64_t t2 = rdtsc();
+            tdiff[k][m] = t2 - t1;
+        }
+        sleep(3);
+    }
+    free(a);
+    free(b);
+    free(c);
+
+    double*     average     = calloc(repm , sizeof(average[0]));
+    uint64_t*   repm_min    = malloc(repm * sizeof(repm_min[0]));
+    uint64_t*   repm_median = malloc(repm * sizeof(repm_median[0]));
+    double*     deviation   = malloc(repm * sizeof(deviation[0]));
+
+    for(unsigned int k = 0 ; k < repm ; k++)
+    {
+        for(unsigned int m = 0 ; m < NB_META ; m++)
+        {
+            average[k] += tdiff[k][m];
+        }
+        average[k] /= NB_META;
+    }
+
+    for(unsigned int k = 0 ; k < repm ; k++)
+    {
+        deviation[k] = 0;
+        for(unsigned int m = 0 ; m < NB_META ; m++)
+        {
+            deviation[k] += pow(average[k] - tdiff[k][m], 2);
+        }
+        deviation[k] /= NB_META;
+        deviation[k] = sqrt(deviation[k]);
+    }
+
+
+    for(unsigned int k = 0 ; k < repm ; k++)
+    {
+        qsort( tdiff[k], NB_META, sizeof tdiff[0][0], cmp_uint64);
+        repm_min[k] = tdiff[k][0];
+        repm_median[k] = tdiff[k][NB_META/2];
+    }
+
+    
+    output = fopen(file_name, "w+");
+    printf("Time per iteration (RDTSC-cycles) :\n");
+    printf("%5s %14s %11s %11s %11s %11s\n", "Step", "Average", "Minimum", "Median", "Stability","Deviation");
+    fprintf(output, "Step   |   Median   |   Stability\n");
+    for(unsigned int k = 0 ; k <  repm ; k++)
+    {
+        const double stabilite = (repm_median[k] - repm_min[k]) * 100.0f / repm_min[k];
+        printf("%5d %14.2lf %11lu %11lu %9.2f%% %9.2f%%\n", k, average[k], repm_min[k], repm_median[k], stabilite, deviation[k]/average[k]*100.0f);
+        fprintf(output, "%d | %lu | %f\n", k, repm_median[k], stabilite);
+    }
+    fclose(output);
+
+    qsort(average, repm, sizeof average[0], cmp_uint64);
+    printf("-----------------------------------------------------\n");
+    printf("\033[1m%s %3s \033[42m%10.2lf\033[0m\n",
+        "Average minimum time per iteration :", "", average[0]);
+
+    printf("\033[1m%s %3s \033[42m%10.2lf\033[0m\n",
+        "Average median time per iteration  :", "", average[(int)(repm/2)]);
+    
+    printf("\033[1m%s %3s \033[42m%10.2lf\033[0m\n",
+        "Average maximum time per iteration :", "", average[repm-1]);
+    printf("-----------------------------------------------------\n");
+
+    for(unsigned int k = 0 ; k < repm ; k++)
+    {
+        free(tdiff[k]);
+    }
+    free(tdiff);
+    free(average);
+    free(repm_min);
+    free(repm_median);
+    return EXIT_SUCCESS;
+}
+
+
